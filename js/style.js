98ab5ea9a246b7db8c116a618e9c1c0fc7711f87
Shoxruh <shoxruhanorboyev0708@gmail.com>
2021-11-28 23:35:25 +0500

    28.1-loyiha

---
+++ js/style.js
@@ -0,0 +1,100 @@
+// let a = prompt('Ismni kiriting:');
+// let b = prompt('Familyani kiriting:');
+// let c = prompt('Tugilgan yilingizni kiriting:');
+// let d = prompt('Telefon nomerni kiriting; ');
+
+// let e = parseInt(c);
+// let g = 2021 - e ;
+
+
+// console.log(a, `:`, b, `/`, g, `/`, d)
+
+
+
+let n = prompt('N sonini kiritng; ')
+let m = prompt('M sonini kiritng; ')
+n = parseInt(n)
+m = parseInt(m)
+let a = Math.pow(n,m);
+let b = Math.random()*n;
+let c = Math.round(b);
+
+let d = n / m ;
+let e =Math.ceil(d);
+let g = Math.floor(d);
+
+
+console.log(a, c, e, g);
+
+
+
+
+
+// let a = 12;
+// let b = '12';
+// console.log(a != b)
+
+
+
+// let a = 12;
+// let b= 13;
+// if(a>b){
+//     console.log(a-b)
+// }else{
+//     console.log(b-a)
+// }
+
+
+
+
+// let a = 13;
+// let b = 18;
+// let d = 11;
+
+// if(a>b && a>d && b>d){
+//     console.log(d, b, a);
+// }else if(b>a && b>d && a>d){
+//     console.log(d, a, b);
+// }else if(d>a && d>b && d>b){
+//     console.log(b, a, d);
+// }else if(a>b && a>d && d>b){
+//     console.log(b, d, a);
+// }else if(b>a && b>d && d>b){
+//     console.log(a, d, b);
+
+// }else{
+//     console.log(a, b, d)
+// }
+
+
+
+// let a = 13;
+// let b = 18;
+// let d = 11;
+
+// if(a>b && a>d){
+
+
+
+//     if(b>d){
+//         console.log(d, b, a);
+//     }else{
+//         console.log(b, d, a);
+//     }
+
+
+// }
+// if( b>a && b>d){
+//     if(a>d){
+//         console.log(d, a, b)
+//     }else{
+//         console.log(a, d, b)
+//     }
+// }
+// if(d>a && d>b){
+//     if(a>b){
+//         console.log(b, a, d)
+//     }else{
+//         console.log(a, b, d)
+//     }
+// }
